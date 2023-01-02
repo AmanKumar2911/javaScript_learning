f5d89840d421e054abc4be16b6b06a5073529f3e
Aman kumar <ang22911@gmail.com>
2023-01-02 12:38:34 +0530

    initial commit

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail="ang2911@gmail.com"
+var accountPassword="1234"
+accountCity ="jaipur"
+let accounState;
+
+// accountId=2  // not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="3454345"
+accountCity="Delhi"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accounState])
